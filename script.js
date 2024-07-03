a9b0e371bec44ff17d599a336e112f01935e702e
Bibhuti-Bhusan-Panda <119800387+Bibhuti-Bhusan-Panda@users.noreply.github.com>
2024-07-03 13:16:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "6c0d75d6939a41edb307472186c357a7";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
